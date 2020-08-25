0c45f7dbb218deb74fbca66ccf2b15adffaab19b
Patrick Dreier <patrick3dreier@googlemail.com>
2020-08-25 16:20:10 +0200

    has_many relationships

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,6 @@
 class Ingredient < ApplicationRecord
+  has_many :preferences
+  has_many :recipe_ingredients
+  has_many :shopping_list_items
+  has_many :recipes, though: :recipe_ingredients
 end
--- app/models/meal_plan.rb
+++ app/models/meal_plan.rb
@@ -1,3 +1,6 @@
 class MealPlan < ApplicationRecord
   belongs_to :user_id
+  has_many :shopping_list_items
+  has_many :meals
+  has_many :recipes, though: :meals
 end
