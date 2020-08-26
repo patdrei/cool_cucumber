32d6bae297356aaeac3a6d9de22b26e94663fb50
Jakob Habsburg <jakob.habsburg@icloud.com>
2020-08-26 16:23:25 +0200

    shopping list without js

--- app/controllers/shopping_list_items_controller.rb
+++ app/controllers/shopping_list_items_controller.rb
@@ -11,3 +11,11 @@ class ShoppingListItemsController < ApplicationController
   def update
   end
 end
+
+
+# spi4 = ShoppingListItem.new(ingredient_id: 90, meal_plan_id:1, purchased: false)
+# spi5 = ShoppingListItem.new(ingredient_id: 60, meal_plan_id:1, purchased: false)
+# spi6 = ShoppingListItem.new(ingredient_id: 50, meal_plan_id:1, purchased: true)
+# spi7 = ShoppingListItem.new(ingredient_id: 40, meal_plan_id:1, purchased: false)
+# spi8 = ShoppingListItem.new(ingredient_id: 170, meal_plan_id:1, purchased: true)
+# spi9 = ShoppingListItem.new(ingredient_id: 270, meal_plan_id:1, purchased: false)
