3e1c3030072203bef07a9515ce37606dac228105
Patrick Dreier <patrick3dreier@googlemail.com>
2020-08-26 12:08:56 +0200

    added preference routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,11 @@ Rails.application.routes.draw do
   resources :recipes, only: [:show]
 
   get "/preferences", to: "preferences#overview", as: "overview"
-  post "/preferences", to: "preferences#preference_set", as: "preference_set"
+  patch "/preferences/:id/deactivate", to: "preferences#deactivate", as: "preference_deactivate"
+
+  post "ingredients/:ingredient_id/preferences", to: "preferences#create_with_ingredient", as: "preference_ingredient"
+
+  post "tags/:tag_id/preferences", to: "preferences#create_with_tag", as: "preference_tag"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_084408) do
+ActiveRecord::Schema.define(version: 2020_08_26_094830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2020_08_25_084408) do
     t.integer "kind"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "active"
     t.index ["ingredient_id"], name: "index_preferences_on_ingredient_id"
     t.index ["tag_id"], name: "index_preferences_on_tag_id"
     t.index ["user_id"], name: "index_preferences_on_user_id"
