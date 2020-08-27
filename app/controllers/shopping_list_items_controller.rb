4aa273b2560837e2efcb62eedb27288c52a1cd88
Jakob Habsburg <jakob.habsburg@icloud.com>
2020-08-27 13:04:03 +0200

    create, without amount

--- app/controllers/shopping_list_items_controller.rb
+++ app/controllers/shopping_list_items_controller.rb
@@ -4,14 +4,33 @@ class ShoppingListItemsController < ApplicationController
     @shopping_list_items = @meal_plan.shopping_list_items
   end
 
+  def create_shopping_list_items
+    # create shopping list item
+    # pass in ingredients  through the recipe_ingredients
+    # same for amount
+    # set purchased to true
+    # check if any of those already exist
+    @meal_plan = MealPlan.find(params[:meal_plan_id])
+    @meal_plan.meals.each do |meal|
+      @recipes = meal.recipes
+      @recipes.recipe_ingredients.each do |recipe_ingredient|
+        ShoppingListItem.create(
+          ingredient_id: recipe_ingredient.ingredient_id,
+          amount: recipe_ingredient.amount,
+          purchased: false,
+          meal_plan_id: params[:meal_plan_id]
+        )
+      end
+    end
+  end
+
   def edit
-    @shopping_list_items = ShoppingListItem.find(params([:id]))
+    @shopping_list_items = ShoppingListItem.mpfind(params([:id]))
   end
 
   def update
   end
 end
-
 
 # spi4 = ShoppingListItem.new(ingredient_id: 90, meal_plan_id:1, purchased: false)
 # spi5 = ShoppingListItem.new(ingredient_id: 60, meal_plan_id:1, purchased: false)
@@ -19,3 +38,5 @@ end
 # spi7 = ShoppingListItem.new(ingredient_id: 40, meal_plan_id:1, purchased: false)
 # spi8 = ShoppingListItem.new(ingredient_id: 170, meal_plan_id:1, purchased: true)
 # spi9 = ShoppingListItem.new(ingredient_id: 270, meal_plan_id:1, purchased: false)
+# MealPlan.find(1).shopping_list_items.find(2).ingredient.recipe_ingredients
+
--- app/javascript/shopping_list_items.js
+++ app/javascript/shopping_list_items.js
@@ -1,28 +1,20 @@
 
 const shopping_list_item_listener = () => {
-  const shopping_list_items = document.querySelectorAll('.shopping_list_item')
-  const shopping_list_items_bought = document.querySelectorAll('.shopping_list_item_bought')
+  const shopping_list_cards = document.querySelectorAll('.shopping_list_item, .shopping_list_item_bought')
+
+  if (shopping_list_items) {
 
 
-
-  shopping_list_items.forEach((shopping_list_item) => {
-    shopping_list_item.addEventListener("click", () => {
-      shopping_list_item.style.backgroundColor = "#F5F5F5";
-      shopping_list_item.style.color = "#EBEBEB";
+    shopping_list_cards.forEach((shopping_list_card) => {
+      shopping_list_card.addEventListener("click", () => {
+        shopping_list_card.classList.toggle("shopping_list_item_bought");
+        shopping_list_card.classList.toggle("shopping_list_item");
+      });
     });
-  });
 
- shopping_list_items_bought.forEach((shopping_list_item) => {
-    shopping_list_item.addEventListener("click", () => {
-      shopping_list_item.style.backgroundColor = "#EBEBEB";
-      shopping_list_item.style.color = "black";
-    });
-  });
-
-
-
-
+  };
 };
+
 export { shopping_list_item_listener };
 
 
