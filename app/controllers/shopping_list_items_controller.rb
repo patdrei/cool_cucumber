ca07d57be31bd4bcc83c71c8472714a330067632
Jakob Habsburg <jakob.habsburg@icloud.com>
2020-08-28 16:16:50 +0200

    shopping list

--- app/controllers/shopping_list_items_controller.rb
+++ app/controllers/shopping_list_items_controller.rb
@@ -4,33 +4,7 @@ class ShoppingListItemsController < ApplicationController
     @shopping_list_items = @meal_plan.shopping_list_items
   end
 
-  def create_shopping_list_items
-    # create shopping list item
-    # pass in ingredients  through the recipe_ingredients
-    # same for amount
-    # set purchased to true
-    # check if any of those already exist
-    @meal_plan = MealPlan.find(params[:meal_plan_id])
-    @meal_plan.meals.each do |meal|
-      @recipes = meal.recipes
-      unless @meal_plan.shopping_list_items.exists?(@recipe.recipe_ingredient.ingredient_id)
-        if ShoppingListItem.exists?(ingredient_id: @recipe.recipe_ingredient.ingredient_id, meal_plan_id: @meal_plan)
-          # calculation
-          find shoppping list item with the same id
-          add the standard units
-        else
-          @recipes.recipe_ingredients.each do |recipe_ingredient|
-            ShoppingListItem.create(
-              ingredient_id: @recipe.recipe_ingredient.ingredient_id,
-              amount: recipe_ingredient.amount,
-              purchased: false,
-              meal_plan_id: params[:meal_plan_id]
-            )
-          end
-        end
-      end
-    end
-  end
+
 
   def edit
     @shopping_list_items = ShoppingListItem.mpfind(params([:id]))
