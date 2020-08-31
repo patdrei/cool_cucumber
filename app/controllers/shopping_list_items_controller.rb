5284e5f13400569b8f0ff7e5df1873c72861575e
Jakob Habsburg <jakob.habsburg@icloud.com>
2020-08-31 14:08:53 +0200

    improved design on the shopping list

--- app/controllers/shopping_list_items_controller.rb
+++ app/controllers/shopping_list_items_controller.rb
@@ -5,9 +5,19 @@ class ShoppingListItemsController < ApplicationController
   end
 
   def edit
-    @shopping_list_items = ShoppingListItem.mpfind(params([:id]))
+    @shopping_list_item = ShoppingListItem.find(params([:id]))
   end
 
   def update
+  end
+
+  def change_purchased
+    @shopping_list_item = ShoppingListItem.find(params([:id]))
+    if @shopping_list_item.purchased == true
+      @shopping_list_item.purchased = false
+    else
+      @shopping_list_item.purchased = true
+    end
+    @shopping_list_item.save
   end
 end
