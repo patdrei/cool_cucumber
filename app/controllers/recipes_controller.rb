43403c0883f5b8f3c76157574c4e95fea58a396e
Patrick Dreier <patrick3dreier@googlemail.com>
2020-09-01 16:11:15 +0200

    recipes are now public

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,6 @@
 class RecipesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:show]
+
   def show
     @recipe = Recipe.find(params[:id])
     @all_recipe_ingredients = @recipe.recipe_ingredients
